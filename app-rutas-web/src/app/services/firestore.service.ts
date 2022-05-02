49d1c977ce10567ac19343eb590aecff36fcb1f0
malvarezg1 <m.alvarezg1@uniandes.edu.co>
2022-05-02 17:29:41 -0500

    Firestore servide created

---
+++ app-rutas-web/src/app/services/firestore.service.ts
@@ -0,0 +1,11 @@
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+
+
+
+export class FirestoreService{
+
+
+}
+
+
