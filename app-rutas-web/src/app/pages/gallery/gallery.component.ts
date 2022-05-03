904d56018c922feb7e3066c8ca7f94f63e244471
Nicolas Bejar <n.bejar@uniandes.edu.co>
2022-05-03 00:04:53 -0500

    cambios

--- app-rutas-web/src/app/pages/gallery/gallery.component.ts
+++ app-rutas-web/src/app/pages/gallery/gallery.component.ts
@@ -1,59 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { MultimediaService } from '../../services/multimedia.service';
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
-
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import * as EXIF from 'exif-js';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-paths',
   templateUrl: './gallery.component.html',
-  styleUrls: [ './gallery.component.css']
+  styleUrls: ['./gallery.component.css']
 
 })
 export class GalleryComponent implements OnInit {
 
   constructor(
-    private multiService: MultimediaService,
-    private sanitizer:DomSanitizer) { }
+    @Inject(DOCUMENT) private document: Document,
+    private _renderer2: Renderer2,
 
+    private multiService: MultimediaService,
+    private sanitizer: DomSanitizer) { }
+  
+  public script = false;
   public image!: SafeUrl;
   public images = new Array<SafeUrl>();
+  
 
-  ngOnInit(): void {
-
-    this.multiService.listImages().then(res=>{
-      console.log(res)
+  async fetchData(): Promise<void>{
+    this.multiService.listImages().then(res => {
       res.items.forEach(element => {
-        console.log(element.name)
         this.displayImage(element.name)
       });
+    });
+  }
+
+
+  ngOnInit(): void {
+    let promesa = this.fetchData();
+
+    promesa.then(res =>{
+      console.log("TERMINO TODO!")
     })
   }
 
-    displayImage(name: String){
-      this.multiService.getImage(name).then(resp =>{
-        console.log(resp)
-        let  string64b = this.arrayBufferToBase64(resp)
-        console.log(string64b)
-        let  url  = this.sanitize("data:image/jpg;base64, " +string64b)
-        console.log(url)
-        this.images.push(url)
-        console.log(this.images)
-      })
-    }
 
-    arrayBufferToBase64( buffer: ArrayBuffer ) {
-      var binary = '';
-      var bytes = new Uint8Array( buffer );
-      var len = bytes.byteLength;
-      for (var i = 0; i < len; i++) {
-         binary += String.fromCharCode( bytes[ i ] );
+  ejemplo(): void{
+    console.log("ENTROO !!!!!")
+    if(this.script == false){
+      this.script = true;
+      //Script
+      let script = this._renderer2.createElement('script');
+      script.text = `
+      {
+          document.getElementById("the-img").onclick = function() {
+          
+            EXIF.getData(this, function() {
+      
+                myData = this;
+      
+                console.log(myData.exifdata);
+            });
+        }
       }
-      return window.btoa( binary );
+      `;
+      this._renderer2.appendChild(this.document.body, script);
     }
+  }
 
-    sanitize( url:string ) {
-      return this.sanitizer.bypassSecurityTrustUrl(url);
+
+  displayImage(name: String) {
+    this.multiService.getImage(name).then(resp => {
+
+      let string64b = this.arrayBufferToBase64(resp)
+      let url = this.sanitize("data:image/jpg;base64, " + string64b)
+      this.images.push(url)
+    });
+        
+
+  }
+
+  arrayBufferToBase64(buffer: ArrayBuffer) {
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
     }
+    return window.btoa(binary);
+  }
+
+  sanitize(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 
 }
 
