3327f7cbc5007a78a202ce8472549866d62fa3f3
malvarezg1 <m.alvarezg1@uniandes.edu.co>
2022-05-02 22:39:09 -0500

    Update

--- app-rutas-web/src/app/pages/gallery/gallery.component.ts
+++ app-rutas-web/src/app/pages/gallery/gallery.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MultimediaService } from '../../services/multimedia.service';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paths',
@@ -13,7 +13,8 @@ export class GalleryComponent implements OnInit {
 
   constructor(
     private multiService: MultimediaService,
-    private sanitizer:DomSanitizer) { }
+    private sanitizer:DomSanitizer,
+    private router: Router) { }
 
   public image!: SafeUrl;
   public images = new Array<SafeUrl>();
@@ -23,7 +24,6 @@ export class GalleryComponent implements OnInit {
     this.multiService.listImages().then(res=>{
       console.log(res)
       res.items.forEach(element => {
-        console.log(element.name)
         this.displayImage(element.name)
       });
     })
@@ -31,13 +31,9 @@ export class GalleryComponent implements OnInit {
 
     displayImage(name: String){
       this.multiService.getImage(name).then(resp =>{
-        console.log(resp)
         let  string64b = this.arrayBufferToBase64(resp)
-        console.log(string64b)
         let  url  = this.sanitize("data:image/jpg;base64, " +string64b)
-        console.log(url)
         this.images.push(url)
-        console.log(this.images)
       })
     }
 
@@ -54,6 +50,10 @@ export class GalleryComponent implements OnInit {
     sanitize( url:string ) {
       return this.sanitizer.bypassSecurityTrustUrl(url);
     }
+
+    btnClick =  () => {
+      this.router.navigateByUrl('/analysis');
+};
 
 }
 
